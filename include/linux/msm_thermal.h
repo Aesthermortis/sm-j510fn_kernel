bca18bd31c0db3d5fa71a35564054d2a64c45bec
Paul Reioux <reioux@gmail.com>
2015-01-17 12:19:39 -0600

    intelli_thermal v3.0: initial coding for Linux 3.10 Qualcomm kernels
    Intelli_thermal is a replacement for the existing Qualcomm msm_thermal engine.
    It allows users greater controls over the stock Qualcomm thermal engine while
    maintaining full backwards compatibilty.
    
    Copyright 2014~2015 Paul Reioux (aka Faux123)
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -146,7 +146,7 @@ struct device_clnt_data {
 	void                         *usr_data;
 };
 
-#ifdef CONFIG_THERMAL_MONITOR
+#if defined (CONFIG_THERMAL_MONITOR) || defined (CONFIG_INTELLI_THERMAL)
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
 extern int msm_thermal_device_init(void);
 extern int msm_thermal_set_frequency(uint32_t cpu, uint32_t freq,
