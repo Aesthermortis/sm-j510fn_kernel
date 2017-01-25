cdbbd843cac057145be738326f69c074498ee0c1
skinzor <robertpopescu95@yahoo.com>
2017-01-25 11:16:06 +0200

    thermal/msm_thermal: add a module param to change the thermal throttling temperature point to userspace
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: flar2 <asegaert@gmail.com>
    
    Conflicts:
    	drivers/thermal/msm_thermal.c

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -52,6 +52,9 @@
 #define SENSOR_SCALING_FACTOR 1
 #define CPU_DEVICE "cpu%d"
 
+unsigned int temp_threshold = 85;
+module_param(temp_threshold, int, 0755);
+
 static struct msm_thermal_data msm_thermal_info;
 static struct delayed_work check_temp_work;
 static bool core_control_enabled;
@@ -2627,7 +2630,7 @@ static void do_freq_control(long temp)
 	if (!freq_table_get)
 		return;
 
-	if (temp >= msm_thermal_info.limit_temp_degC) {
+	if (temp >= temp_threshold) {
 		if (limit_idx == limit_idx_low)
 			return;
 
@@ -2635,7 +2638,7 @@ static void do_freq_control(long temp)
 		if (limit_idx < limit_idx_low)
 			limit_idx = limit_idx_low;
 		max_freq = table[limit_idx].frequency;
-	} else if (temp < msm_thermal_info.limit_temp_degC -
+	} else if (temp < temp_threshold -
 		 msm_thermal_info.temp_hysteresis_degC) {
 		if (limit_idx == limit_idx_high)
 			return;
