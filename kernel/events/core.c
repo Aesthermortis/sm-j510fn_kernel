75c7fa03b3d1ef76daf2d93b1686d8020e1cbe57
demicro <figdido@gmail.com>
2017-01-27 22:21:15 +0100

    Upgrade to 3.10.56

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1402,6 +1402,11 @@ retry:
 	 */
 	if (ctx->is_active) {
 		raw_spin_unlock_irq(&ctx->lock);
+		/*
+		 * Reload the task pointer, it might have been changed by
+		 * a concurrent perf_event_context_sched_out().
+		 */
+		task = ctx->task;
 		goto retry;
 	}
 
@@ -1833,6 +1838,11 @@ retry:
 	 */
 	if (ctx->is_active) {
 		raw_spin_unlock_irq(&ctx->lock);
+		/*
+		 * Reload the task pointer, it might have been changed by
+		 * a concurrent perf_event_context_sched_out().
+		 */
+		task = ctx->task;
 		goto retry;
 	}
 
