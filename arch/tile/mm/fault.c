388010e16e5d4389ca4668c41dd24c0648920c19
demicro <figdido@gmail.com>
2017-02-12 16:28:01 +0100

    linux: 3.10.61

--- arch/tile/mm/fault.c
+++ arch/tile/mm/fault.c
@@ -571,15 +571,10 @@ no_context:
  */
 out_of_memory:
 	up_read(&mm->mmap_sem);
-	if (is_global_init(tsk)) {
-		yield();
-		down_read(&mm->mmap_sem);
-		goto survive;
-	}
-	pr_alert("VM: killing process %s\n", tsk->comm);
-	if (!is_kernel_mode)
-		do_group_exit(SIGKILL);
-	goto no_context;
+	if (is_kernel_mode)
+		goto no_context;
+	pagefault_out_of_memory();
+	return 0;
 
 do_sigbus:
 	up_read(&mm->mmap_sem);
