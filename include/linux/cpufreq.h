abb62fcd768c39dfdc97e9b42660f62a0f24a53b
demicro <figdido@gmail.com>
2017-02-13 22:59:03 +0100

    cpufreq: governor lionfish

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -481,6 +481,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
 extern struct cpufreq_governor cpufreq_gov_barry_allen;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
+extern struct cpufreq_governor cpufreq_gov_lionfish;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_lionfish)
 #endif
 
 /*********************************************************************
