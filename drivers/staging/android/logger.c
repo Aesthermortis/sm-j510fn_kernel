c76724c2808dbc2a743c0ddb7d907f8bd07d1636
demicro <figdido@gmail.com>
2017-03-16 19:33:10 +0100

    logger: Add an toggle for logging
    Signed-off-by: Francisco Franco
    <franciscofranco.1990@gmail.com>

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -45,6 +45,9 @@ static char klog_buf[256];
 #define CONFIG_LOGCAT_SIZE 256
 #endif
 
+static unsigned int enabled = 0;
+module_param(enabled, uint, S_IWUSR | S_IRUGO);
+
 /**
  * struct logger_log - represents a specific log, such as 'main' or 'radio'
  * @buffer:	The actual ring buffer
@@ -504,6 +507,9 @@ static ssize_t logger_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	struct logger_entry header;
 	struct timespec now;
 	ssize_t ret = 0;
+
+        if (!enabled)
+                return 0;
 
 	now = current_kernel_time();
 
